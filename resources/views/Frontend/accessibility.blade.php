960ac6f07ea1181dcdf9deab28c5e3f1782d2ba4
Nextus Marketing <marketingteamnextus@gmail.com>
2025-10-30 21:04:35 +0530

    changes done

--- resources/views/Frontend/accessibility.blade.php
+++ resources/views/Frontend/accessibility.blade.php
@@ -102,7 +102,7 @@
     Los Angeles, CA 90094</p>
 
     <p>comparehomesecurity.org is committed to making our website's content accessible and user friendly to everyone. If you are having difficulty viewing or navigating the content on this website, or notice any content, feature, 
-        or functionality that you believe is not fully accessible to people with disabilities, please call our Customer Service team at 855-957-4069 or email our team at customerservice@comparehomesecurity.org 
+        or functionality that you believe is not fully accessible to people with disabilities, please call our Customer Service team at 855-957-4069 or email our team at info@comparehomesecurity.org 
         with "Disabled Access" in the subject line and provide a description of the specific feature you feel is not fully accessible or a suggestion for improvement. 
         We take your feedback seriously and will consider it as we evaluate ways to accommodate all of our customers and our overall accessibility policies. Additionally, while we do not control such vendors, 
         we strongly encourage vendors of third-party digital content to provide content that is accessible and user friendly.</p>
--- resources/views/Frontend/disclosure.blade.php
+++ resources/views/Frontend/disclosure.blade.php
@@ -104,7 +104,7 @@
     <p>The comparehomesecurity.org editorial team is committed to delivering the most current resources in home and personal security. We want to keep offering our services for free and without ads, so at times, we do link to products and include phone numbers of service providers we review. We may earn compensation when you purchase a product or service from certain advertising partners. We may also earn commissions from clicks or inquiries on certain partners.</p>
 
     <p>comparehomesecurity.org is committed to making our website's content accessible and user friendly to everyone. If you are having difficulty viewing or navigating the content on this website, or notice any content, feature, 
-        or functionality that you believe is not fully accessible to people with disabilities, please call our Customer Service team at 855-957-4069 or email our team at customerservice@comparehomesecurity.org 
+        or functionality that you believe is not fully accessible to people with disabilities, please call our Customer Service team at 855-957-4069 or email our team at info@comparehomesecurity.org 
         with "Disabled Access" in the subject line and provide a description of the specific feature you feel is not fully accessible or a suggestion for improvement. 
         We take your feedback seriously and will consider it as we evaluate ways to accommodate all of our customers and our overall accessibility policies. Additionally, while we do not control such vendors, 
         we strongly encourage vendors of third-party digital content to provide content that is accessible and user friendly.</p>
