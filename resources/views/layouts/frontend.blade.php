0324cb3dc2d0617669572234fd6e16ae534c3574
Nextus Marketing <marketingteamnextus@gmail.com>
2025-11-07 02:07:51 +0530

    changes done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -24,6 +24,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"/>
 	<link href="/frontend/css/custom.css" rel="stylesheet" media="screen">
+  <link rel="canonical" href="{{ url()->current() }}" />
+
 </head>
 <body>
 <style>
