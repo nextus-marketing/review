b89d9dd178653e4a33e0c20e75f083863f06f9dd
Nextus Marketing <marketingteamnextus@gmail.com>
2026-01-13 00:45:12 +0530

    changes done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -285,9 +285,9 @@
                 d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.21 11.36 11.36 0 003.54.57 1 1 0 011 1V20a1 1 0 01-1 1A17 17 0 013 5a1 1 0 011-1h3.5a1 1 0 011 1 11.36 11.36 0 00.57 3.54 1 1 0 01-.45 1.25l-2.2 2.2z" />
         </svg>
     <!-- Scroll to Top Button -->
-    <button id="scrollTopBtn" title="Go to top">
+    <!-- <button id="scrollTopBtn" title="Go to top">
       <i class="fa-solid fa-arrow-up"></i>
-    </button>
+    </button> -->
 
     <script>
       // Show or hide the button
